80914f65e9d17f6935da8fbbf8c76535602a7bbf
Logan Mayo <lmayo@nightcrawler.highpoint.edu>
2019-01-27 17:15:41 -0500

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Logan Mayo on 1/27/19.
+//  Copyright © 2019 Logan Mayo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
